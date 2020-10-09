c35d7dec33b074b2cf217ac483655188d04dd765
Pirer12 <pirer23@gmail.com>
2020-10-09 20:43:54 +0200

    fix bug - return empty objects & config mssql

--- src/main/java/pl/pirer/carapp/dao/entity/Car.java
+++ src/main/java/pl/pirer/carapp/dao/entity/Car.java
@@ -1,11 +1,17 @@
 package pl.pirer.carapp.dao.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Car {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
